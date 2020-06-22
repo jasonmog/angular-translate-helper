53f5b91056738df99df78ef4777004af0954de55
Jason Jaskolka <jjaskolka@granburyrs.com>
2020-06-22 15:28:46 -0700

    Parse translation file to correctly add JSON entry and reformat

--- src/main/java/LocalizeAction.java
+++ src/main/java/LocalizeAction.java
@@ -1,20 +1,29 @@
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.codeInsight.actions.ReformatCodeProcessor;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Caret;
-import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileChooser.FileChooser;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.ReadonlyStatusHandler;
+import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
 import com.intellij.util.Consumer;
 import org.jetbrains.annotations.NotNull;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import static com.intellij.openapi.updateSettings.impl.pluginsAdvertisement.PluginsAdvertiser.NOTIFICATION_GROUP;
 
 public class LocalizeAction extends AnAction {
     @Override
@@ -32,31 +41,89 @@ public class LocalizeAction extends AnAction {
         promptForFile(project, new Consumer<VirtualFile>() {
             @Override
             public void consume(VirtualFile virtualFile) {
-                final String text = getSelectedText(editor);
-                final String translationId = getTranslationId(text);
-                final String newText = "{{ '" + translationId + "'  | translate }}";
+                if (virtualFile != null && !virtualFile.isDirectory()) {
+                    final String text = getSelectedText(editor);
+                    String translationId = getTranslationId(text);
 
-                replaceText(editor, project, newText);
-                addTranslationEntry(project, virtualFile, translationId, text);
+                    try {
+                        translationId = addTranslationEntry(project, virtualFile, translationId, text);
+
+                        final String newText = "{{ '" + translationId + "' | translate }}";
+                        replaceText(editor, project, newText);
+                    } catch (Exception exception) {
+                        exception.printStackTrace();
+
+                        showNotification(project, exception.getMessage());
+                    }
+                }
             }
         });
     }
 
-    static void addTranslationEntry(Project project, VirtualFile file, String translationId, String text) {
-        final Document document = FileDocumentManager.getInstance().getDocument(file);
-        final int closingBraceIndex = document.getText().lastIndexOf("\n};");
-        final String entry = ",\n    " + translationId + ": '" + text + "'";
+    static void showNotification(Project project, String text) {
+        final Notification notification = NOTIFICATION_GROUP.createNotification(text, NotificationType.ERROR);
+        notification.notify(project);
+    }
 
-        ReadonlyStatusHandler.OperationStatus status = ReadonlyStatusHandler.getInstance(project).ensureFilesWritable(List.of(file));
+    static String addTranslationEntry(Project project, VirtualFile file, String translationId, String text) throws IOException, ParseException, Exception {
+        final ReadonlyStatusHandler.OperationStatus status = ReadonlyStatusHandler.getInstance(project).ensureFilesWritable(List.of(file));
 
         if (status.hasReadonlyFiles()) {
-            return;
+            throw new IOException("File is read only.");
         }
 
+        JSONObject json = (JSONObject)new JSONParser().parse(new InputStreamReader(file.getInputStream()));
+
+        final Object value = json.get(translationId);
+
+        if (value instanceof String) {
+            if (value.equals(text)) {
+                return translationId;
+            }
+
+            // TODO: increment until unique
+            translationId += "_2";
+
+            if (json.get(translationId) != null) {
+                throw new Exception("Translation ID conflict.");
+            }
+        }
+
+        json.put(translationId, text);
+
+        // TODO: fix
+        // json = sortJSONObject(json);
+
+        writeJSONToVirtualFile(project, json, file, true);
+
+        return translationId;
+    }
+
+    static void writeJSONToVirtualFile(Project project, JSONObject json, VirtualFile file, boolean reformat) throws Exception {
         WriteCommandAction.runWriteCommandAction(project, new Runnable() {
             @Override
             public void run() {
-                document.insertString(closingBraceIndex, entry);
+                try {
+                    VfsUtil.saveText(file, json.toJSONString());
+                } catch (IOException e) {
+                    // TODO: throw higher
+
+                    e.printStackTrace();
+
+                    showNotification(project, e.getMessage());
+                }
+
+                if (reformat) {
+                    try {
+                        formatFile(project, file);
+                    } catch (Exception e) {
+                        // TODO: throw higher
+
+                        e.printStackTrace();
+
+                        showNotification(project, e.getMessage());
+                    }
+                }
             }
         });
     }
@@ -87,6 +154,39 @@ public class LocalizeAction extends AnAction {
     }
 
     static String getTranslationId(String text) {
-        return text.toUpperCase().replaceAll(" ", "_");
+        return text.toUpperCase().replaceAll("[^A-Za-z]]", "_");
+    }
+
+    static void formatFile(Project project, VirtualFile file) throws Exception {
+        final PsiFile psiFile = PsiManager.getInstance(project).findFile(file);
+
+        if (psiFile == null) {
+            throw new Exception("Unable to create PsiFile.");
+        }
+
+        final ReformatCodeProcessor processor = new ReformatCodeProcessor(project, psiFile, null, false);
+        processor.run();
+    }
+
+    static JSONObject sortJSONObject(JSONObject obj) {
+        final ArrayList<String> list = new ArrayList<>();
+        final JSONObject result = new JSONObject();
+
+        for(Object key : obj.keySet()) {
+            list.add((String)key);
+        }
+
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return a.compareTo(b);
+            }
+        });
+
+        for (String key : list) {
+            result.put(key, obj.get(key));
+        }
+
+        return result;
     }
 }

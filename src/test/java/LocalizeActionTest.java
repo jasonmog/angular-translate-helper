e8112b0d701f437c142efe557fdb4208a0422aa1
Jason Jaskolka <jjaskolka@granburyrs.com>
2020-06-23 12:30:21 -0700

    Add unit test for getTranslationId

---
+++ src/test/java/LocalizeActionTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LocalizeActionTest {
+    @Test
+    public void testGetTranslationId() {
+        final String result = LocalizeAction.getTranslationId(".foo-bar. apple.");
+        final String expected = "FOO_BAR_APPLE";
+
+        Assert.assertEquals(result, expected);
+    }
+}
